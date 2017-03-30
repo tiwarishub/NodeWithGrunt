d8f0eea1b12c7fd826e23b01ffea7e892f51717f
Disha Khakhar <dikhakha@microsoft.com>
2017-03-30 13:31:44 +0530

    demo

---
+++ gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+
+    const fs = require("fs");
+
+
+  // A very basic default task.
+  grunt.registerTask('default', 'Log some stuff.', function() {
+    console.log("yayyyy");
+  });
+};
